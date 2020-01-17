c75842169f6ccfb9d82bcc982b7991a91b6054ae
attachai27 <attachaisaorangtoi@gmail.com>
2020-01-17 16:27:26 +0700

    create and importing component

---
+++ my-app/src/components/Simple.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Simple extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Simple</h2>
+            </div>
+        )
+    }
+}
+
+export default Simple
